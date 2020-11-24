fb09b3b726366b428e820ced245b6e5acd415dde
HarpeBlue <hspe2507@gmail.com>
2020-11-24 10:52:36 -0500

    iniital commit: variables, tipos de datos

---
+++ valiables.js
@@ -0,0 +1,23 @@
+var nombre = 'Harold';
+
+//declarar
+
+var edad;
+
+//inicializar
+
+edad = 30;
+
+edad
+var elementos = ['Computadora', 'celular' ]; //declarando e inicializando
+
+var persona = {
+  nombre: 'Diego',
+  edad: 30,
+}
+
+persona;
+
+//podemos llamar un elemento de un array abriendo cochete y dentro lponemos el número del índice empezando por cero
+
+elementos[1];
---
+++ valores.js
@@ -0,0 +1,17 @@
+
+// Valores primitivos
+/*
+  number = 40
+  string = 'Hola yo soy Harold'
+  booleano = true or false, validar cosas
+
+  valores vacíos = null, undefined. Valores placeholder
+*/
+
+// Valores no primitivos o tipo objeto.
+/*
+  Array = [a,b,c,d]
+  Object = { nombre: 'Diego' }
+*/
+
+//Con typeof + tipo de dato, me da el tipo de dato perse
