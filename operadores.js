c76e37c63cb539c133ce1d4fcd6c9ccf17ae8225
HarpeBlue <hspe2507@gmail.com>
2020-11-26 13:07:44 -0500

    add: operadores.

---
+++ operadores.js
@@ -0,0 +1,37 @@
+// de operación, operadores binarios.
+
+3 + 2
+50 - 10
+10 * 20
+20 / 2
+
+"Harold " + "Peñaloza" // usar el mas para hacer concatenaciones de strings
+
+!false // operadores unitarios, este sirve para negar.
+
+var a = 1 // operador de asignación.
+
+3 == "3"; // asignar no, sino comparar. compara no tanto el tipo de valor sino el valor persé
+
+3 === "3" // compara tanto el tipo como el valor persé, por lo tanto esto es false.
+
+
+5 < 3 // compara cuál es menor
+
+5 > 3 // compara si es mayor.
+
+5 <= 6 // menor o igual
+
+5 >= 6 // mayor o igual.
+
+a && b // comparar o validar que las dos sean válidas. y
+
+a || b // cualquiera de las dos se puéde cumplir. o
+
+// para incrementar
+
+var edad = 24;
+
+edad++;
+
+edad += 2; // incrementar con mas de uno
