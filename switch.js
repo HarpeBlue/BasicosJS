24e394871851141e54e04d3e9da93830314a31e6
HarpeBlue <hspe2507@gmail.com>
2020-12-12 19:28:57 -0500

    add: condicional switch.

---
+++ switch.js
@@ -0,0 +1,14 @@
+//Valida una condición que funciona por casos
+
+var numero = 1;
+
+switch (numero) {
+  case 1:
+    console.log("Soy un uno!");
+    break;
+  case 10:
+    console.log("Soy un 10");
+    break;
+  default:
+    console.log("No soy nada!");
+}
